ff024b336dc3bb88d89caac22959791b4a5e09af
MMhunter <kengtou.mh@antfin.com>
2018-11-24 14:42:26 +0800

    init

--- index.js
+++ index.js
@@ -3,27 +3,56 @@
  X          Y     Meaning                            TEST CASES
  -------------------------------------------------
  [AMD]   not updated
- M        [ MD]   updated in index                 M_.js  MM.js  MD.js
- A        [ MD]   added to index                   A_.js  AM.js  AD.js
- D                deleted from index               D_.js
- R        [ MD]   renamed in index                 R_1.js -> R_2.js    RM1.js->RM2.js  RD1.js->RD2.js
+ M        [ MD]   updated in index                 M_.txt  MM.txt  MD.txt
+ A        [ MD]   added to index                   A_.txt  AM.txt  AD.txt
+ D                deleted from index               D_.txt
+ R        [ MD]   renamed in index                 R_1.txt -> R_2.txt    RM1.txt->RM2.txt  RD1.txt->RD2.txt
  C        [ MD]   copied in index
  [MARC]           index and work tree matches
- [ MARC]     M    work tree changed since index    _M.js
- [ MARC]     D    deleted in work tree             _D.js
+ [ MARC]     M    work tree changed since index    _M.txt
+ [ MARC]     D    deleted in work tree             _D.txt
  [ D]        R    renamed in work tree
  [ D]        C    copied in work tree
  -------------------------------------------------
- D           D    unmerged, both deleted          DD.js
- A           U    unmerged, added by us           AU.js
- U           D    unmerged, deleted by them       UD.js
- U           A    unmerged, added by them         UA.js
- D           U    unmerged, deleted by us         DU.js
- A           A    unmerged, both added            AA.js
- U           U    unmerged, both modified         UU.js
+ D           D    unmerged, both deleted          DD.txt
+ A           U    unmerged, added by us           AU.txt
+ U           D    unmerged, deleted by them       UD.txt
+ U           A    unmerged, added by them         UA.txt
+ D           U    unmerged, deleted by us         DU.txt
+ A           A    unmerged, both added            AA.txt
+ U           U    unmerged, both modified         UU.txt
  -------------------------------------------------
- ?           ?    untracked                       ??.js
- !           !    ignored                         !!.js
+ ?           ?    untracked                       ??.txt
+ !           !    ignored                         !!.txt
  -------------------------------------------------
  */
+
+const child_process = require('child_process');
+const fs = require('fs');
+
+function runGit(args) {
+  child_process.execSync('git', args);
+}
+
+function stage(file) {
+  runGit(['stage', '--', file]);
+}
+
+/**
+ * CLEAN
+ */
+runGit(['reset','--hard']);
+
+/**
+ * TEST CASES GENERATION
+ */
+
+/**
+ * M_.txt
+ */
+fs.writeFileSync('M_.txt','M_.txt content')
+
+
+
+
 
